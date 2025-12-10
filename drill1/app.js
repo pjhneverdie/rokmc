424de63f247dd66058d7bb1973dc12b8a0095b89
JINHYEOK PARK <pjhneverdie@gmail.com>
2025-12-10 11:14:55 +0000

    2025-12-10 20:15 폼 생성, 기본 이벤트 조정

---
+++ drill1/app.js
@@ -0,0 +1,20 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(e) {
+    e.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    const username = loginInput.value;
+
+    greeting.innerText = "Hello " + username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+
+loginForm.addEventListener("submit", onLoginSubmit);
