08f93c3089144a262852f5d1e31d9ce4b419d8c9
JINHYEOK PARK <pjhneverdie@gmail.com>
2025-12-13 11:32:57 +0000

    스프링부트 프로젝트 생성

---
+++ drill2/payutil/src/main/java/pjh/drill/payutil/PayutilApplication.java
@@ -0,0 +1,13 @@
+package pjh.drill.payutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayutilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayutilApplication.class, args);
+	}
+
+}
---
+++ drill2/payutil/src/test/java/pjh/drill/payutil/PayutilApplicationTests.java
@@ -0,0 +1,13 @@
+package pjh.drill.payutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayutilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
