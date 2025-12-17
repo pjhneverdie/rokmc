49a4362d9ad4d73c2c2f91c4651d5cfe861d5aea
JINHYEOK PARK <pjhneverdie@gmail.com>
2025-12-17 11:01:33 +0000

    깃 캐시 초기화

--- drill1/js/background.js
+++
@@ -1,9 +0,0 @@
-const iamges = ["0.jpeg", "1.jpeg", "2.jpeg"];
-
-const chosenImage = iamges[Math.floor(Math.random() * iamges.length)];
-
-const bgImages = document.createElement("img");
-
-bgImages.src = `/drill1/img/${chosenImage}`;
-
-document.body.appendChild(bgImages);
