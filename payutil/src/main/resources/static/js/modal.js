8f229d87d2c435744292cfcdaaeb2dcb4353b27c
JINHYEOK PARK <pjhneverdie@gmail.com>
2025-12-31 11:48:46 +0000

    레이아웃 수정 중 모바일 퍼스트!

--- payutil/src/main/resources/static/js/modal.js
+++ payutil/src/main/resources/static/js/modal.js
@@ -1,11 +1,16 @@
 const modal = document.getElementById("modal");
 const shareBtn = document.getElementById("share-btn");
 const closeBtn = document.getElementById("close-btn");
+const overlay = document.getElementById('modal-overlay');
 
 shareBtn.addEventListener("click", () => {
   modal.classList.add("is-open");
+
 });
 
 closeBtn.addEventListener("click", () => {
+
+
   modal.classList.remove("is-open");
 });
+
