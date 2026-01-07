2e8355a5aa9f049b20d7ee593aa7f2d59564d155
JINHYEOK PARK <pjhneverdie@gmail.com>
2026-01-07 11:23:46 +0000

    jpa 엔티티 맵핑 연습용 프로젝트 생성, mysql 연결까지 초기 설정 완료

---
+++ jpadrill/src/main/java/com/pjh/jpadrill/JpadrillApplication.java
@@ -0,0 +1,13 @@
+package com.pjh.jpadrill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpadrillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpadrillApplication.class, args);
+	}
+
+}
---
+++ jpadrill/src/test/java/com/pjh/jpadrill/JpadrillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjh.jpadrill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpadrillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
